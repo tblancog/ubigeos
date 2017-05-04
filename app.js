bd92b9c6370a92c84e9747b33a74cd9dbb3515d7
Tony Blanco <tblanco@cablevision.com.ar>
2017-05-04 17:55:38 -0300

    * CSV to Array function * Get element code and and name

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-function process() {
-  var data = '  “01 Lima /  / ”\
-    “01 Lima / 50 Lima / ”\
-    “01 Lima / 51 Barranca / ”\
-    “01 Lima / 50 Lima / 202 La Molina”\
-    “01 Lima / 50 Lima / 203 San Isidro”\
-    “02 Arequipa /  / ”\
-    “02 Arequipa / 63 Arequipa / ”\
-    “02 Arequipa / 64 Caylloma / ”\
-    “02 Arequipa / 63 Arequipa / 267 Cercado”\
-  ';
+function csvToArray() {
 
-  
+var csvString = '"01 Lima /  / "\n\
+"01 Lima / 50 Lima / "\n\
+"01 Lima / 51 Barranca / "\n\
+"01 Lima / 50 Lima / 202 La Molina"\n\
+"01 Lima / 50 Lima / 203 San Isidro"\n\
+"02 Arequipa /  / "\n\
+"02 Arequipa / 63 Arequipa / "\n\
+"02 Arequipa / 64 Caylloma / "\n\
+"02 Arequipa / 63 Arequipa / 267 Cercado"';
+
+  var arrayCsv = csvString.split( "\n" );
+
+  var dptos = [];
+  // Loop each line
+  for( i=0; i < arrayCsv.length; i++ ) {
+
+    // Replace quotes
+    var line = arrayCsv[i].replace(/['"]/g, "");
+
+    // Split by dash
+    var arrayDash= line.split("/")
+    console.log(arrayDash);
+
+
+    // Get departamento
+    var dpto= getElement(arrayDash[0])
+    dptos.push();
+
+  }
+  console.log(dptos);
+
+}
+
+function getElement( text ) {
+  var cod = text.match(/\d/g).join("");
+  var name = text.match(/[A-Za-z]+\w|\s/g).join("").trim();
+  return {
+    "cod": cod,
+    "name": name
+  };
 }
